56372ef99e37fe747c44f3796fa3a6f61b29c3ee
rafaa1999 <raferhouma10@gmail.com>
2022-09-23 18:48:01 +0100

    use token with to seperate services

--- article/index.js
+++ article/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken")
+// jwt is popular in the microservice system
 const app = express();
 
 app.use(express.json());
@@ -7,11 +8,24 @@ app.use(express.json());
 const articles = [];
 
 app.get("/api/1.0/articles", (req, res) => {
+  // get all articles
   res.send(articles);
 })
 
 app.post("/api/1.0/articles", (req, res) => {
-  // 
+  // check the existence of token
+  const authorization = req.headers.authorization
+  // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYzOTUzNjgwfQ.rbbQu77lZQIqMRIhi-95zozTimUGlC5M2v2iCCeJhFo'
+  // take the token from the header
+   const token = authorization.substring(7)
+   // verify the token
+   const result = jwt.verify(token,"this_is_the_secret_payload") // return the payload
+
+   const article = req.body
+   article.userId = result.id // take the user id from token(result) and pass it in the creation of article 
+
+   articles.push(article)
+   res.send({message : "success"})
 })
 
 app.listen(3001, () => {
--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken")
 
 const app = express();
 
@@ -16,7 +17,20 @@ for(let i = 1; i <= 5; i++) {
 }
 
 app.post("/api/1.0/auth", (req, res) => {
-  //
+  const credential = req.body
+  const user = users.find(user => user.email == credential.email) // find the user that already exist
+  if(!user){
+    res.status(401).send({message : 'unauthorized email!!'}) 
+  }
+  if(user.password !== credential.password){
+    res.status(401).send({message : 'unauthorized password!!'}) 
+  }
+  // if email exist and password correct the we generate a token
+  // create the payload
+  const token = jwt.sign({id : user.id}, "this_is_the_secret_payload" )
+  return res.send({
+    token : token
+  })
 })
 
 
